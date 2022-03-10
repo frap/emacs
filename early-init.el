9fba8943433b4ff6887180289a6eff288100dbb4
FRAP <frap@tuatara.red>
2022-03-11 10:36:11 +1300

    Added prosiletaous modus-themes prot-common, prot-simple, fonts, orderless, search and part of minibuffer (no mct)

---
+++ early-init.el
@@ -0,0 +1,97 @@
+;;; early-init.el --- Early Customisations -*- lexical-binding: t; no-byte-compile: t; -*-
+;; Copyright (C) 2021 Gas
+
+;; Author: Gas <gas@tuatara.red>
+;; Version: 1.0
+;; Package-Version: 0.1
+;; Created: Sometime during the Covid-19 lockdown
+;; Keywords: configuration, emacs
+;; URL: https://github.com/frap/emacs.d
+;; Package-Requires: ((emacs "27.2"))
+
+;; Timestamp: <>
+;; This file is not part of GNU Emacs.
+
+;;; Commentary:
+;; This file is automatically tangled from config.org.
+;; Hand edits will be overwritten!
+;; Je t'ai prévenu putain!
+
+;;; Code:
+
+(message "Chargement en cours early-init.el...")
+
+(define-advice load (:before (feature &rest _))
+    "Message the user when loading a library."
+    (with-temp-message (format "En cours de chargement de la bibliothèque: '%s'" feature)))
+
+;;; ============================================================================
+;;; Emacs startup optimisations
+;;; ============================================================================
+;; Garbage collection slows down startup time, so we maximise the threshold for
+;; it to run, and we will later reset it.
+(let ((normal-gc-cons-threshold (* 20 1024 1024))  ;; 20mb
+      (init-gc-cons-threshold (* 128 1024 1024)))    ;;128mb
+  (setq gc-cons-threshold init-gc-cons-threshold)
+  (add-hook 'emacs-startup-hook
+            (lambda () (setq gc-cons-threshold
+                             normal-gc-cons-threshold))))
+;;(setq gc-cons-threshold most-positive-fixnum)
+
+;; file-name-handler-alist is consulted on various I/O functions such as
+;; REQUIRE, slowing down startup time, so we set it to NIL, and establish a hook
+;; to restore when Emacs is finished starting.
+(unless (or (daemonp) noninteractive)
+  (let ((file-name-handler-alist/old file-name-handler-alist))
+    (setq file-name-handler-alist nil)
+    (add-hook 'emacs-startup-hook
+              (lambda ()
+                (let ((value (delete-dups
+                              (append file-name-handler-alist
+                                      file-name-handler-alist/old))))
+                  (setq file-name-handler-alist value))))))
+
+(unless (daemonp)
+  (advice-add #'tty-run-terminal-initialization :override #'ignore)
+  (add-hook 'window-setup-hook
+            (lambda ()
+              (advice-remove #'tty-run-terminal-initialization #'ignore)
+              (tty-run-terminal-initialization (selected-frame) nil t))))
+
+
+;;; ============================================================================
+;;; Frame parameters for faster startup
+;;; ============================================================================
+(setq-default
+ initial-frame-alist '((width . 170)
+                       (height . 56)
+                       (tool-bar-lines . 0)
+                       (bottom-divider-width . 0)
+                       (right-divider-width . 1)
+                       (font . "Iosevka Slab 12"))
+ default-frame-alist initial-frame-alist
+ frame-inhibit-implied-resize t            ;; dont resize
+ frame-resize-pixelwise t                   ;; as GUI use pixels
+ x-gtk-resize-child-frames 'resize-mode
+ fringe-indicator-alist (assq-delete-all 'truncation fringe-indicator-alist))
+
+ (setq truncate-partial-width-windows nil)
+
+
+(when (fboundp #'tool-bar-mode)
+    (tool-bar-mode -1))
+
+(when (fboundp #'scroll-bar-mode)
+     (scroll-bar-mode -1))
+
+
+(when (featurep 'native-compile)
+  (defvar native-comp-deferred-compilation)
+  (setq native-comp-deferred-compilation t)
+  (defvar native-comp-async-report-warnings-errors)
+  (setq native-comp-async-report-warnings-errors nil))
+
+
+(message "Chargement du early-init.el terminé!")
+(provide 'early-init)
+;;; early-init.el ends here
---
+++ init.el
@@ -0,0 +1,124 @@
+;;; init.el --- Gas Init -*- lexical-binding: t -*-
+  ;; Copyright (C) 2021 Gas
+  
+  ;; Author: Gas <gas@tuatara.red>
+  ;; Version: 1.0
+  ;; Package-Version: 0.1
+  ;; Created: Sometime during the Covid-19 lockdown
+  ;; Keywords: configuration, emacs
+  ;; URL: https://github.com/frap/emacs.d
+  ;; Package-Requires: ((emacs "27.2"))
+  
+  ;; Timestamp: <>
+  ;; This file is not part of GNU Emacs.
+  
+  ;;; Commentary:
+  ;; This file is automatically tangled from config.org.
+  ;; Hand edits will be overwritten!
+  ;; Je t'ai prévenu putain!
+  
+  ;;; Code:
+    (defmacro +with-message (message &rest body)
+      "Execute BODY, with MESSAGE.
+		    If body executes without errors, ** MESSAGE... terminé will be displayed."
+      (declare (indent 1))
+      (let ((msg (gensym)))
+	`(let ((,msg ,message))
+	   (unwind-protect (progn (message "%s..." ,msg)
+				  ,@body)
+	     (message "** %s... terminé!!" ,msg)))))
+
+  ;;; Set up extra load paths and functionality
+  ;; Since we might be running in CI or other environments, stick to
+  ;; XDG_CONFIG_HOME value if possible.
+  (let ((emacs-home/ (if-let ((xdg (getenv "XDG_CONFIG_HOME")))
+			   (expand-file-name "emacs/" xdg)
+			 user-emacs-directory)))
+  ;; Add Lisp directory to `load-path'.
+  (add-to-list 'load-path (expand-file-name "lisp" emacs-home/)))
+
+;;; Bootstrap
+;;; ============================================================================
+;;; Specify the directory paths 
+;;; ============================================================================
+  (require 'config-path)
+
+;;; ============================================================================
+;;; Set up the package manager
+;;; ============================================================================
+  (require 'init-elpa)
+
+;;; ============================================================================
+;;; Specify the directory paths 
+;;; ============================================================================
+  
+  (setup (:require prot-common))
+  
+  ;;; Initialise Gas Constants
+    (defconst ENV-IS-NATIVECOMP? (if (fboundp 'native-comp-available-p) (native-comp-available-p)))
+    (defconst ENV-IS-GRAPHIC? (display-graphic-p))
+    (defconst ENV-IS-MAC?     (eq system-type 'darwin))
+    (defconst ENV-IS-LINUX?   (eq system-type 'gnu/linux))
+    (defconst ENV-IS-TERMUX?
+      (string-suffix-p "Android" (string-trim (shell-command-to-string "uname -a"))))
+    (defconst env-sys-name (system-name))
+  
+    (defmacro fn (&rest body)
+      `(lambda () ,@body))
+  
+    (defun doom-enlist (exp)
+      "Return EXP wrapped in a list, or as-is if already a list."
+      (declare (pure t) (side-effect-free t))
+      (if (proper-list-p exp) exp (list exp)))
+
+;; Setup `custom-file`.
+  (setq custom-file (concat emacs-config/ "custom.el"))
+
+  ;; load autoloads file
+  (unless elpa-bootstrap-p
+      (unless (file-exists-p path-autoloads-file)
+	(error "Le fichier autoloads n'existe pas, veuillez exécuter '%s'"
+	       "eru install emacs"))
+      (load path-autoloads-file nil 'nomessage))
+
+;;; core
+(require 'init-env)
+(require 'init-kbd)
+(require 'init-startup)
+(require 'init-fn-macros)
+(require 'init-editor)
+(require 'init-ui)
+(require 'init-buffer)
+(require 'init-window)
+;;; utilities
+(require 'init-selection)
+(require 'init-project)
+(require 'init-vcs)
+;;(require 'enfer-pkg-builtin)
+
+;;; languages
+(require 'init-ide)
+(require 'init-lisp)
+(require 'init-elisp)
+(require 'init-clisp)
+(require 'init-clojure)
+(require 'init-fennel)
+(require 'init-ess)
+(require 'init-utils)
+
+;; Org & Roam
+(require 'init-notes)
+(require 'init-file-templates)
+
+;;; user config & some defaults
+(require 'init-usersetup)
+(require 'init-sanity)
+
+;; I don't use `customize' interface, but .dir-locals.el put 'safe'
+;; variables into `custom-file'. And to be honest, I hate to allow
+;; them every time I restart Emacs.
+(when (file-exists-p custom-file)
+  (load custom-file nil 'nomessage))
+
+(provide 'init)
+;;; init.el ends here
